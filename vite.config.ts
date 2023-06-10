c41e35ae4374bcabd712426acf917ccdba6c77c9
fish-liqueur <ispeakwithdead@gmail.com>
2023-06-10 17:54:40 +0400

    Fixes for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import {fileURLToPath, URL} from "node:url";
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+    base: '/tetris/',
     resolve: {
+
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
             '@game': fileURLToPath(new URL('./src/game', import.meta.url)),
