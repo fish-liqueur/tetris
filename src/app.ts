7c1d66c01929baf19f43df95bdc76fa300eada0e
fish-liqueur <ispeakwithdead@gmail.com>
2023-08-11 23:43:03 +0400

    fix template

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,10 @@
 import { Game } from "@game/game";
+import CanvasGamepad from 'canvas-gamepad';
 
 new Game();
+
+new CanvasGamepad('controls', onTouchButton);
+
+function onTouchButton(buttonKey: string): void {
+    console.log('onTouchButton ', buttonKey);
+}
